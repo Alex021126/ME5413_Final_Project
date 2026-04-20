f8a6914273768c92a80741ccef12716b6f58046d
Alex Zuo <alex@AlexdeMacBook-Air.local>
2026-04-20 19:53:38 +0800

    Remove generated build artifacts

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/sam/ME5413_Final_Project/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/sam/ME5413_Final_Project/devel;/opt/ros/noetic'.split(';') if '/home/sam/ME5413_Final_Project/devel;/opt/ros/noetic' != '' else []
